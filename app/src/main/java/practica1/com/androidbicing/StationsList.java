7a66e2d415ae2dc44cc995021d3089fdda3b083c
Sergi <sergibca@gmail.com>
2016-01-20 19:52:48 +0100

    Initial commit

---
+++ app/src/main/java/practica1/com/androidbicing/StationsList.java
@@ -0,0 +1,54 @@
+
+package practica1.com.androidbicing;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class StationsList {
+
+    @SerializedName("stations")
+    @Expose
+    private List<Station> stations = new ArrayList<Station>();
+    @SerializedName("updateTime")
+    @Expose
+    private Integer updateTime;
+
+    /**
+     * 
+     * @return
+     *     The stations
+     */
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    /**
+     * 
+     * @param stations
+     *     The stations
+     */
+    public void setStations(List<Station> stations) {
+        this.stations = stations;
+    }
+
+    /**
+     * 
+     * @return
+     *     The updateTime
+     */
+    public Integer getUpdateTime() {
+        return updateTime;
+    }
+
+    /**
+     * 
+     * @param updateTime
+     *     The updateTime
+     */
+    public void setUpdateTime(Integer updateTime) {
+        this.updateTime = updateTime;
+    }
+
+}
